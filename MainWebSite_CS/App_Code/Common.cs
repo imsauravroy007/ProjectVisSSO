50e54ec721af27e0cd5c2eb0b364d972f8ae0f07
imsauravroy007 <rsaurav81@yahoo.com>
2015-09-08 01:38:58 -0700

    C sharp version of sts Server

---
+++ MainWebSite_CS/App_Code/Common.cs
@@ -0,0 +1,11 @@
+﻿
+
+/// <summary>
+/// Summary description for Common
+/// </summary>
+public static class Common
+{
+    public const string IssuerName = "IssuerName";
+    public const string SigningCertificateName = "SigningCertificateName";
+    public const string EncryptingCertificateName = "EncryptingCertificateName";
+}
---
+++ MainWebSite_CS/Login.aspx.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Web.Security;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load( object sender, EventArgs e )
+    {
+        // Note: Add code to validate user name, password. This code is for illustrative purpose only.
+        // Do not use it in production enviro
+        
+
+        if ( !string.IsNullOrEmpty( txtUserName.Text ) )
+        {
+            if ( Request.QueryString["ReturnUrl"] != null )
+            {
+                FormsAuthentication.RedirectFromLoginPage( txtUserName.Text, false );
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie( txtUserName.Text, false );
+                Response.Redirect( "default.aspx" );
+            }
+        }
+        else if ( !IsPostBack )
+        {
+            //txtUserName.Text = "Adam Carter";
+        }
+    }    
+}
